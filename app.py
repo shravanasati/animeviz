d437e95bacfe23b5974ec1068dbc7092ef2437fd
Shravan Asati <dev.shravan@protonmail.com>
2024-01-25 23:24:57 +0530

    setup project

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.get("/ping")
+def ping():
+    return {"ping": "pong"}
+
+
+@app.get("/")
+def home():
+    return render_template("index.html")
