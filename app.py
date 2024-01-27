017bd25684652a4245d6565bcd315c8f40986173
Shravan Asati <dev.shravan@protonmail.com>
2024-01-27 17:16:49 +0530

    minor styling change

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY") or os.urandom(24)
 
 
 @app.get("/ping")
@@ -11,6 +13,12 @@ def ping():
 @app.get("/")
 def home():
     return render_template("index.html")
+
+
+@app.get("/login")
+def login():
+    # todo replace this with oauth code
+    return render_template("login.html")
 
 
 @app.get("/visualize")
